ddb9eb5614dbab0132139771c1c71e0e090c8467
Ted Jenkler <tedjenkler@gmail.com>
2024-12-06 07:49:50 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 
-COMMANDS = []
+COMMANDS = ['exit 0']
 
 def main():
 
@@ -12,6 +12,8 @@ def main():
     if command not in COMMANDS:
         print(f"{command}: command not found")
         main()
+    elif command == "exit 0":
+        exit(0)
 
 if __name__ == "__main__":
     main()
