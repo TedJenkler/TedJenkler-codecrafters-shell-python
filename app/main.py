f0d19ccb7db94c5fae6a2c3f6ace0865e67355aa
Ted Jenkler <tedjenkler@gmail.com>
2024-12-06 07:28:53 +0100

    Error handling unknown commands

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 import sys
 
+COMMANDS = []
 
 def main():
 
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
+    command = input()
 
+    if command not in COMMANDS:
+        print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
