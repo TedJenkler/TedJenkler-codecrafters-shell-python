6dccbf7c5d2820b43ea00309af1277bf35627efd
Ted Jenkler <tedjenkler@gmail.com>
2024-12-08 17:46:50 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 
-COMMANDS = ['exit', 'echo']
+COMMANDS = ['exit', 'echo', 'type']
 
 def main():
 
@@ -17,6 +17,12 @@ def main():
         exit(0)
     elif splitted_command[0] == "echo":
         print(" ".join(splitted_command[1:]))
+        main()
+    elif splitted_command[0] == "type":
+        if splitted_command[1] in COMMANDS:
+            print(f"{splitted_command[1]} is a shell builtin")
+        else:
+            print(f"{splitted_command[1]}: not found")
         main()
 
 if __name__ == "__main__":
