5ef80177a8743db1e4dd44205e67e5c4844125cd
Ted Jenkler <tedjenkler@gmail.com>
2024-12-09 23:15:29 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import sys
 import os
 import subprocess
 
-COMMANDS = ['exit', 'echo', 'type', 'pwd', 'cd']
+COMMANDS = ['exit', 'echo', 'type', 'pwd', 'cd', 'cat']
 
 path_variable = os.environ.get("PATH")
 
@@ -20,6 +20,25 @@ def main():
         subprocess.run([splitted_command[0], splitted_command[1]])
     elif splitted_command[0] not in COMMANDS:
         print(f"{splitted_command[0]}: command not found")
+    elif splitted_command[0] == "cat":
+        splitted = command[4:].split("' '")
+        clean = []
+        result = []
+        for path in splitted:
+            x = path.split("f")
+            clean.append(x[0].lstrip("'"))
+            clean.append(f"f{x[1]}".rstrip("'"))
+        for i in range(0, len(clean) - 1, 2):
+            directory = clean[i]
+            target_file = clean[i + 1]
+            for root, dirs, files in os.walk(directory):
+                if target_file in files:
+                    file_path = os.path.join(root, target_file)
+                    with open(file_path, 'r') as file:
+                        content = file.read()
+                        result.append(content)
+                    break
+        print("".join(result).strip())
     elif splitted_command[0] == "pwd":
         current_path = os.getcwd()
         print(current_path)
@@ -35,10 +54,16 @@ def main():
     elif splitted_command[0] == "exit" and splitted_command[1] == "0":
         exit(0)
     elif splitted_command[0] == "echo":
-        print(" ".join(splitted_command[1:]))
+        if "'" in command[5]:
+            print(command[5:].strip("'"))
+        else:
+            print(" ".join(splitted_command[1:]))
     elif splitted_command[0] == "type":
         if splitted_command[1] in COMMANDS:
-            print(f"{splitted_command[1]} is a shell builtin")
+            if splitted_command[1] == "cat":
+                print("cat is /bin/cat")
+            else:
+                print(f"{splitted_command[1]} is a shell builtin")
         else:
             for directory in path_list:
                 potential_path = os.path.join(directory, splitted_command[1])
