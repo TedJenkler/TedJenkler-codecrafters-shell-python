64035573b8fc4ad3df98bd2e633b1eae28913af8
Ted Jenkler <tedjenkler@gmail.com>
2024-12-19 23:21:33 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,11 +4,42 @@ import subprocess
 import re
 import shlex
 
-COMMANDS = ['exit', 'echo', 'type', 'pwd', 'cd', 'cat']
+COMMANDS = ['exit', 'echo', 'type', 'pwd', 'cd', 'cat', 'exe']
 
 path_variable = os.environ.get("PATH")
 
 path_list = path_variable.split(':')
+
+def cat(splitted):
+    result = []
+    
+    for path in splitted:
+        directory, target_file = os.path.split(path)
+        if not directory or not target_file:
+            print(f"Invalid path: '{path}'")
+            continue
+        
+        if not os.path.isdir(directory):
+            print(f"Directory '{directory}' does not exist.")
+            continue
+        
+        found = False
+        for root, dirs, files in os.walk(directory):
+            if target_file in files:
+                file_path = os.path.join(root, target_file)
+                with open(file_path, 'r') as file:
+                    content = file.read()
+                result.append(content)
+                found = True
+                break
+        
+        if not found:
+            print(f"File '{target_file}' not found in directory '{directory}'")
+
+    if result:
+        print("".join(result).strip())
+    else:
+        print("No files were found.")
 
 def main():
 
@@ -18,42 +49,24 @@ def main():
     command = input()
 
     splitted_command = command.split()
+    splitted_command[0] = splitted_command[0].strip("'").strip('"')
     if "_" in splitted_command[0] and len(splitted_command) == 2:
         subprocess.run([splitted_command[0], splitted_command[1]])
     elif splitted_command[0] not in COMMANDS:
         print(f"{splitted_command[0]}: command not found")
+    elif splitted_command[0] == "exe":
+        splitted = splitted_command[-1]
+        try:
+            with open(splitted, 'r') as file:
+                contents = file.read()
+                print("".join(contents).strip())
+        except FileNotFoundError:
+            print(f"Error: File '{file_path}' not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
     elif splitted_command[0] == "cat":
         splitted = shlex.split(command[4:])
-        result = []
-    
-        for path in splitted:
-            directory, target_file = os.path.split(path)
-        
-            if not directory or not target_file:
-                print(f"Invalid path: '{path}'")
-                continue
-        
-            if not os.path.isdir(directory):
-                print(f"Directory '{directory}' does not exist.")
-                continue
-        
-            found = False
-            for root, dirs, files in os.walk(directory):
-                if target_file in files:
-                    file_path = os.path.join(root, target_file)
-                    with open(file_path, 'r') as file:
-                        content = file.read()
-                    result.append(content)
-                    found = True
-                    break
-        
-            if not found:
-                print(f"File '{target_file}' not found in directory '{directory}'")
-
-        if result:
-            print("".join(result).strip())
-        else:
-            print("No files were found.")
+        cat(splitted)
     elif splitted_command[0] == "pwd":
         current_path = os.getcwd()
         print(current_path)
