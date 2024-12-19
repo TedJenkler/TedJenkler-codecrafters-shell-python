666698da57654ba52cc5041bd0ba7c1b7a476360
Ted Jenkler <tedjenkler@gmail.com>
2024-12-19 23:37:13 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,9 +10,12 @@ path_variable = os.environ.get("PATH")
 
 path_list = path_variable.split(':')
 
+ """
+    Command CAT
+ """
+
 def cat(splitted):
     result = []
-    
     for path in splitted:
         directory, target_file = os.path.split(path)
         if not directory or not target_file:
@@ -41,6 +44,51 @@ def cat(splitted):
     else:
         print("No files were found.")
 
+    """ 
+    Command EXE
+    """
+
+    def exe(splitted):
+        try:
+            with open(splitted, 'r') as file:
+                contents = file.read()
+                print("".join(contents).strip())
+        except FileNotFoundError:
+            print(f"Error: File '{file_path}' not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def pwd():
+        current_path = os.getcwd()
+        print(current_path)
+
+    def cd(path)
+        if new_path == '~':
+            home_directory = os.environ.get('HOME')
+            os.chdir(home_directory)
+        elif os.path.isdir(new_path):
+            os.chdir(new_path)
+        else:
+            print(f"cd: {new_path}: No such file or directory")
+
+    def echo():
+        x = shlex.split(command, posix=True)
+        print(" ".join(x[1:]))
+
+    def type_command():
+        if splitted_command[1] in COMMANDS:
+            if splitted_command[1] == "cat":
+                print("cat is /bin/cat")
+            else:
+                print(f"{splitted_command[1]} is a shell builtin")
+        else:
+            for directory in path_list:
+                potential_path = os.path.join(directory, splitted_command[1])
+                if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
+                    print(potential_path)
+                    main()
+            print(f"{splitted_command[1]}: not found")
+
 def main():
 
     sys.stdout.write("$ ")
@@ -56,48 +104,21 @@ def main():
         print(f"{splitted_command[0]}: command not found")
     elif splitted_command[0] == "exe":
         splitted = splitted_command[-1]
-        try:
-            with open(splitted, 'r') as file:
-                contents = file.read()
-                print("".join(contents).strip())
-        except FileNotFoundError:
-            print(f"Error: File '{file_path}' not found.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+        exe(splitted)
     elif splitted_command[0] == "cat":
         splitted = shlex.split(command[4:])
         cat(splitted)
     elif splitted_command[0] == "pwd":
-        current_path = os.getcwd()
-        print(current_path)
+        pwd()
     elif splitted_command[0] == "cd":
         new_path = splitted_command[1]
-        if new_path == '~':
-            home_directory = os.environ.get('HOME')
-            os.chdir(home_directory)
-        elif os.path.isdir(new_path):
-            os.chdir(new_path)
-        else:
-            print(f"cd: {new_path}: No such file or directory")
+        cd(new_path)
     elif splitted_command[0] == "exit" and splitted_command[1] == "0":
         exit(0)
     elif splitted_command[0] == "echo":
-        x = shlex.split(command, posix=True)
-        print(" ".join(x[1:]))
+        echo(command)
     elif splitted_command[0] == "type":
-        if splitted_command[1] in COMMANDS:
-            if splitted_command[1] == "cat":
-                print("cat is /bin/cat")
-            else:
-                print(f"{splitted_command[1]} is a shell builtin")
-        else:
-            for directory in path_list:
-                potential_path = os.path.join(directory, splitted_command[1])
-                if os.path.isfile(potential_path) and os.access(potential_path, os.X_OK):
-                    print(potential_path)
-                    main()
-            print(f"{splitted_command[1]}: not found")
-
+        type_command()
     main()
 
 if __name__ == "__main__":
